386d5a439733c79a8d5c7332a7cfdc5d872580e4
SystematicError <systematicerror@users.noreply.github.com>
2026-07-12 18:36:10 +0100

    feat: handle chess960 parsing

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use std::io::{BufRead, stdin};
 use std::process::exit;
 
 fn main() {
-    let chess960 = false;
+    let mut chess960 = false;
     let mut engine = Engine::new();
 
     for line in stdin().lock().lines() {
@@ -16,6 +16,7 @@ fn main() {
                 Uci::Uci => {
                     println!("id name Updog");
                     println!("id author SystematicError");
+                    println!("option name UCI_Chess960 type check default false");
                     println!("option name Threads type spin default 1 min 1 max 1");
                     println!("option name Hash type spin default 1 min 1 max 1");
                     println!("uciok");
@@ -25,7 +26,20 @@ fn main() {
 
                 Uci::NewGame => todo!(),
 
-                Uci::SetOption(_name, _value) => todo!(),
+                Uci::SetOption(name, value) => {
+                    // TODO: Use try blocks instead of IIFE
+                    (|| {
+                        match name.as_str() {
+                            "UCI_Chess960" => chess960 = value?.parse().ok()?,
+                            "Threads" => todo!(),
+                            "Hash" => todo!(),
+
+                            _ => {}
+                        }
+
+                        Some(())
+                    })();
+                }
 
                 Uci::Position(board, moves) => engine.set_position(board, moves),
 
