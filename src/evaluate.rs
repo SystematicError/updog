6212765e194c5f94bdec118b7191669f280c5c4d
SystematicError <systematicerror@users.noreply.github.com>
2026-07-26 23:48:29 +0100

    feat: material evaluation (#6)
    STC: Pass
    
    Results of material-evaluation (exp) vs master (base) (8+0.08, 1t, 1MB, UHO_4060_v4.epd):
    Elo: 572.55 +/- 58.09, nElo: 1263.54 +/- 30.33
    LOS: 100.00 %, DrawRatio: 0.40 %, PairsRatio: inf
    Games: 504, Wins: 469, Losses: 1, Draws: 34, Points: 486.0 (96.43 %)
    Ptnml(0-2): [0, 0, 1, 34, 217], WL/DD Ratio: inf
    LLR: 2.95 (100.1%) (-2.94, 2.94) [0.00, 3.00]
    
    Bench: 70779604

--- src/evaluate.rs
+++ src/evaluate.rs
@@ -1,4 +1,4 @@
-use cozy_chess::Board;
+use cozy_chess::{Board, Color, Piece};
 use std::fmt;
 
 pub type Evaluation = i32;
@@ -28,7 +28,39 @@ impl fmt::Display for EvaluationDisplay {
     }
 }
 
-// TODO: Implement evaluation
-pub fn evaluate(_board: &Board) -> Evaluation {
-    0
+fn piece_value(piece: Piece) -> Evaluation {
+    match piece {
+        Piece::King => 20000,
+        Piece::Queen => 900,
+        Piece::Rook => 500,
+        Piece::Bishop => 330,
+        Piece::Knight => 320,
+        Piece::Pawn => 100,
+    }
+}
+
+pub fn evaluate(board: &Board) -> Evaluation {
+    let mut score = 0;
+
+    for piece in [
+        Piece::Queen,
+        Piece::Rook,
+        Piece::Bishop,
+        Piece::Knight,
+        Piece::Pawn,
+    ] {
+        // Material score
+
+        let white_pieces = board.colored_pieces(Color::White, piece).len() as Evaluation;
+        let black_pieces = board.colored_pieces(Color::Black, piece).len() as Evaluation;
+
+        score += piece_value(piece) * (white_pieces - black_pieces);
+    }
+
+    let perspective = match board.side_to_move() {
+        Color::White => 1,
+        Color::Black => -1,
+    };
+
+    score * perspective
 }
