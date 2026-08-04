0e502f734745197842a8fcdae742c25925ad7d42
SystematicError <systematicerror@users.noreply.github.com>
2026-08-04 22:12:19 +0100

    feat: decrease bench depth
    Bench: 572338

--- src/bench.rs
+++ src/bench.rs
@@ -6,7 +6,7 @@ use std::sync::Arc;
 use std::sync::atomic::AtomicBool;
 use std::time::{Duration, Instant};
 
-const DEPTH: Ply = 5;
+const DEPTH: Ply = 4;
 
 const POSITIONS: [&str; 50] = [
     "r3k2r/2pb1ppp/2pp1q2/p7/1nP1B3/1P2P3/P2N1PPP/R2QK2R w KQkq a6 0 14",
